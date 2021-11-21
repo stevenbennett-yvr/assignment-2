186bd52b4bdda8b75d5efa2b84c5d67d653f6064
Steven <stevenbennett90@gmail.com>
2021-11-20 16:18:44 -0800

    Have the cleanUpIndex function working as intended. Create Sinlge list is giving me trouble.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-/* This file is where all your work should go for Assignment 2.  Please remove this comment. */
+function cleanUpIndex() {
+    let contact = document.getElementsByClassName('contact');
+    while(contact[0]) {
+        contact[0].parentNode.removeChild(contact[0]);
+    }
+}
+
+
+function createSingleIndex(input) {
+    var new_card = document.createElement('div');
+    new_card.className = "contact";
+    new_card.innerHMTL = input;
+    document.body.appendChild(new_card)
+    return new_card
+}
+
+function renderIndex(input)
+    
