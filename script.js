4940ecb360d0ef5e199d90282d09c2f65743efcf
stevenbennettyvr <stevenbennett90@gmail.com>
2021-11-21 16:20:04 -0800

    got renderIndex to function properly. learned dictionary in python is called an object in javascript and that a list is called an array. definitions are important for figuring out what to do.

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+let contactList = [
+    {
+      name: "Roberta Dobbs",
+      phone: "778-555-1234",
+      address: "101 Main St, Anytown, USA",
+      email: "subgenius@slack.example.com",
+    }, 
+    {
+      name: "Bugs Bunny",
+      phone: "123-867-5309",
+      address: "Warner Brothers Animation Lot",
+      email: "whatsup@doc.example.com",
+    },
+  ]
+
 function cleanUpIndex() {
     let contact = document.getElementsByClassName('contact');
     while(contact[0]) {
@@ -7,25 +22,28 @@ function cleanUpIndex() {
 
 
 function createSingleIndex(input) {
-    /* <a href="page3.html"><div class="contact"><p>Barry Allen</p></div></a> */
+    /* define elements <a href="page3.html"><div class="contact"><p>Barry Allen</p></div></a> */
     main = document.querySelectorAll('div.main');
     text = document.createTextNode(input);
     paragraph = document.createElement('p');
     div = document.createElement('div');
     a = document.createElement('a');
+    /* insert class=contact into div and href=page3.html into a */
     div.classList.add('contact');
     a.href = "page3.html";
+    /* put the parst togeather */
     paragraph.appendChild(text);
     div.appendChild(paragraph);
     a.appendChild(div);
+    /* append into list */
     main[0].appendChild(a);
     return a
 }
 
-function renderIndex(input){
-    /* append onto <div class='main'> list */
-    new_card = createSingleIndex(input);
-    return main
+function renderIndex(contactlist) {
+    /* accepts array of objects, isolates data from object and inputs it into createSingleIndex */
+    for (let i = 0; i < contactlist.length; i++)
+        createSingleIndex(contactlist[i].name);
 }
 
 function cleanUpView(){
